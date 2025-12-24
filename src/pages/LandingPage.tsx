8c3a98fe0b47d7466ad73432b7bc973d24ef21ac
ArnavScripts <arnavmehta.thelegend@gmail.com>
2025-12-25 01:20:54 +0530

    fixed image

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -102,7 +102,7 @@ export default function LandingPage() {
                                 <div className="w-3 h-3 rounded-full bg-green-500/50" />
                             </div>
                             <img
-                                src="./landingPageImg.png"
+                                src="/landingPageImg.png"
                                 alt="Dashboard Interface"
                                 className="w-full h-auto opacity-90"
                             />
